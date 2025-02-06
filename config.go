09d2b4d9029d854983ffe9796710f3aabe8cec82
公明 <83812544+Ed1s0nZ@users.noreply.github.com>
2025-02-06 15:01:05 +0800

    Update config.go

--- config.go
+++ config.go
@@ -1,8 +1,10 @@
 package main
 
 const (
-	apiKey  = "sk-xxx" // kimi 的 apikey
-	cookie2 = "cookie2" // cookie2
+	apiKeyKimi     = "sk-xxxxxxx" // 替换为你的kimi API key
+	apiKeyDeepSeek = "sk-yyyyyyy"                 // 替换为你的deepseek API key
+	AI             = "deepseek"                                            // 可选择deepseek或kimi
+	cookie2        = "cookie2"
 )
 
-var suffixes = []string{".js", ".ico", ".png", ".jpg", "jpeg"} //这些后缀的接口不扫
+var suffixes = []string{".js", ".ico", ".png", ".jpg", "jpeg"} //这些后缀不扫
