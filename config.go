947a299f7b1187ea0df58355aef3b7610190c758
公明 <83812544+Ed1s0nZ@users.noreply.github.com>
2025-02-19 11:17:25 +0800

    Update config.go

--- config.go
+++ config.go
@@ -7,4 +7,26 @@ const (
 	cookie2        = "cookie2"
 )
 
-var suffixes = []string{".js", ".ico", ".png", ".jpg", "jpeg"} //这些后缀不扫
+var suffixes = []string{
+	// 静态资源文件
+	".js", ".ico", ".png", ".jpg", ".jpeg", ".gif", ".bmp", ".svg", ".ttf", ".woff", ".woff2", ".eot", ".otf",
+	".mp3", ".wav", ".ogg", ".mp4", ".webm", ".avi", ".css", ".scss", ".less",
+
+	// 临时文件和缓存文件
+	".tmp", ".temp", ".cache", ".swp",
+
+	// 日志文件
+	".log", ".err",
+
+	// 配置文件
+	".env", ".json", ".yml", ".yaml", ".xml", ".ini",
+
+	// 编译生成文件
+	".class", ".dll", ".so", ".zip", ".tar", ".gz",
+
+	// 其他常见文件
+	".txt", ".md", ".doc", ".docx", ".pdf", ".csv", ".xlsx", ".xls", ".sh", ".bat",
+
+	// 特殊用途文件
+	".gitignore", ".lock", ".bak",
+} //这些后缀不扫
